7b88c9a9944139d84bd815543993e24b03435769
am499702@gmail.com <am499702@gmail.com>
2024-10-18 11:24:12 +0530

    Routing between Billing and Express

--- index.js
+++ index.js
@@ -24,5 +24,20 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+//This code will run for production
+
+if(process.env.NODE_ENV==="production"){
+    //Express will serve up production assets
+    //Like our main.js or main.css file
+    app.use(express.static('client/build'))
+
+    //Express will serve up the index.html
+    //if it doesnt work up /surveys
+    const path=require('path')
+    app.length('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    })
+}
+
 const PORT= process.env.PORT || 5000
 app.listen(PORT)
