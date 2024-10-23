288eebf25bd0319c01ca4a9ec428964c562653bb
am499702@gmail.com <am499702@gmail.com>
2024-10-23 12:46:22 +0530

    Mailgun redirection logic created

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,4 @@
+const keys=require('../../config/keys')
 module.exports = (survey) => {
     return (
     `
@@ -6,8 +7,8 @@ module.exports = (survey) => {
             <div style="text-align:center">
                 <h3> I'd like your input </h3>
                 <p>${survey.body}</p>
-                <div> <a href=""> Yes </a> </div>
-                <div> <a href="http://localhost:3000"> No </a> </div>
+                <div> <a href="${keys.redirectDomain}/api/surveys/thanks"> Yes </a> </div>
+                <div> <a href="${keys.redirectDomain}/api/surveys/thanks"> No </a> </div>
             </div>
         </body>
     </html>
