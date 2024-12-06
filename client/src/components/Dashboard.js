304749908b67904375eb147a268da195526fba43
am499702@gmail.com <am499702@gmail.com>
2024-12-06 20:44:01 +0530

    Added  a text to the landing page

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,7 +6,12 @@ const Dashboard=()=>{
     return (
         <div>
             <SurveyList/>
+            <div style={{textAlign:'center'}}><h1> Welcome to the Emaily!</h1> </div>
+            <div style={{textAlign:'center'}}> <h4>Click Below to send out a survey</h4> </div>
             <div className="fixed-action-btn">
+                 
+
+                   
                 
                     <Link to="/surveys/new" className='btn-floating btn-large red' style={{position:'absolute',bottom:'50px',right:'50px'}}>
                     <i class="material-icons">
